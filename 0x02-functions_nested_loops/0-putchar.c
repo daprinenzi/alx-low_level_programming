9d63244c5b07314cfd59f5331efa20d4afa3b42f
Akintunde Dolapo <40601040+daprinenzi@users.noreply.github.com>
2022-03-15 18:33:36 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "putchar.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  prints putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
