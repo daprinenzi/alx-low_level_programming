c09f9a609abcc79cc3740a388e7f81a77199fcf0
Akintunde Dolapo <40601040+daprinenzi@users.noreply.github.com>
2022-03-21 18:16:43 +0100

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
