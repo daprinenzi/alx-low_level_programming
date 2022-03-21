764791b9770b0ba0cd03c9e6f6cb9a3b96912900
Akintunde Dolapo <40601040+daprinenzi@users.noreply.github.com>
2022-03-21 18:26:10 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,43 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <main.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
  *
- * Return: Always 0 (success)
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, j, k, s;
-char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char p[58];
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-srand(time(NULL));
-while (s != 2772)
-{
-i = k = s = 0;
-while ((2772 - 122) > s)
-{
-j = rand() % 62;
-p[i] = c[j];
-s += c[j];
-i++;
-}
-while (c[K])
-{
-if (c[K] == (2772 - s))
-{
-p[i] = c[k];
-s += c[k];
-i++;
-break;
-}
-k++;
-}
-p[i] = '\0';
-printf("%s", p);
-return (0);
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
 }
